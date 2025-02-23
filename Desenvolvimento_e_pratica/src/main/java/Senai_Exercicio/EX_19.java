d9710df283d7f5c31cb7ad0b6f9855b1018ceaa8
Bruno Souza Cruz <brunoscb4@gmail.com>
2025-02-23 20:44:07 -0300

    Senai trabalho

--- Desenvolvimento_e_pratica/src/main/java/Senai_Exercicio/EX_19.java
+++ Desenvolvimento_e_pratica/src/main/java/Senai_Exercicio/EX_19.java
@@ -1,4 +1,8 @@
 package Senai_Exercicio;
 
+import java.util.Random;
+
 public class EX_19 {
+    public static void main(String[] args) {
+    }
 }
--- Desenvolvimento_e_pratica/src/main/java/Senai_Exercicio/EX_20.java
+++ Desenvolvimento_e_pratica/src/main/java/Senai_Exercicio/EX_20.java
@@ -1,4 +1,9 @@
 package Senai_Exercicio;
 
+import java.util.Random;
+
+
 public class EX_20 {
+
+
 }
