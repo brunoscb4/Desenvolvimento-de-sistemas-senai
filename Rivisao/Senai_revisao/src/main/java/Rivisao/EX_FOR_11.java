e9191e8a1166e07a3cc37289687ae64d4e27de9c
brunoscb4 <108295116+brunoscb4@users.noreply.github.com>
2025-02-25 20:05:56 -0300

    Delete Rivisao directory

--- Rivisao/Senai_revisao/src/main/java/Rivisao/EX_FOR_11.java
+++
@@ -1,9 +0,0 @@
-package Rivisao;
-
-public class EX_FOR_11 {
-    public static void main(String[] args) {
-        for (int i = 1; i <10 ; i++) {
-            System.out.println(i*i);
-        }
-    }
-}
